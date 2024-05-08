5b113c1a61dbe2058708fa0b71019b080021224a
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-08 11:50:21 +0530

    logic of fund function

--- FundMe.sol
+++ FundMe.sol
@@ -6,7 +6,15 @@
 pragma solidity ^0.8.18;
 
 contract FundMe{
-    function fund() public{}
+    function fund() public payable{//1. How do we send ETH to this contract? - Use "payable"
+        // Allow users to send $
+        // Have a minimum $ sent
+        require(msg.value>1e18, "didn't send enough ETH");// 1e18 = 1ETH = 1000000000000000000 = 1 * 10 ** 18
+
+        // What is a revert?
+        // Undo any actions that have been done, and send the remaining gas back
+        // Even if the transaction reverts, since some computation occured. Gas is spent
+    }
 
     function withdraw() public{}
 }
