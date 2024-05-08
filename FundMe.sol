ddf79ef092a488dde1cfb6edfa8383478e3e6dd7
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-08 11:23:10 +0530

    project setup

---
+++ FundMe.sol
@@ -0,0 +1,12 @@
+//Get fund from users
+//Withdraw funds
+//Set a minimum funding value in USD
+
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+
+contract FundMe{
+    function fund() public{}
+
+    function withdraw() public{}
+}
