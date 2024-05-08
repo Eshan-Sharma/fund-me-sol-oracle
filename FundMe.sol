dcd6f0bdea5d75af64a8c94c53ea9e517bb799d0
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-08 15:50:51 +0530

    create library and move code

--- FundMe.sol
+++ FundMe.sol
@@ -5,9 +5,10 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe{
+    using PriceConverter for uint256;
     uint256 public minimumUsd = 5 * 1e18;//Instead of minimum ETH, let's set minimum Usd value
     address[] public funders;
     mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
@@ -15,7 +16,8 @@ contract FundMe{
     function fund() public payable{//1. How do we send ETH to this contract? - Use "payable"
         // Allow users to send $
         // Have a minimum $ sent
-        require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough ETH");// 1e18 = 1ETH = 1000000000000000000 = 1 * 10 ** 18
+        
+        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH");// 1e18 = 1ETH = 1000000000000000000 = 1 * 10 ** 18
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
         // What is a revert?
@@ -24,26 +26,6 @@ contract FundMe{
 
 
        
-    }
-    // Using Oracle to get the price of ETH/USD
-    function getPrice() public view returns (uint256){
-        //Address - 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        //ABI 
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-         (
-            /* uint80 roundID */,
-            int price,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-        ) = priceFeed.latestRoundData();
-        return uint256(price) * 1e10;
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
     }
 
     function withdraw() public{}
---
+++ PriceConverter.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter{
+    // Using Oracle to get the price of ETH/USD
+    function getPrice() internal view returns (uint256){
+        //Address - 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //ABI 
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+         (
+            /* uint80 roundID */,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return uint256(price) * 1e10;
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
