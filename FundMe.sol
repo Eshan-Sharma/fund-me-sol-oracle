725869d12d399376f98af07330c9888e3763a4f8
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-08 15:36:32 +0530

    keeping an array of fund senders

--- FundMe.sol
+++ FundMe.sol
@@ -9,11 +9,15 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 
 contract FundMe{
     uint256 public minimumUsd = 5 * 1e18;//Instead of minimum ETH, let's set minimum Usd value
+    address[] public funders;
+    mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
 
     function fund() public payable{//1. How do we send ETH to this contract? - Use "payable"
         // Allow users to send $
         // Have a minimum $ sent
         require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough ETH");// 1e18 = 1ETH = 1000000000000000000 = 1 * 10 ** 18
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
         // What is a revert?
         // Undo any actions that have been done, and send the remaining gas back
         // Even if the transaction reverts, since some computation occured. Gas is spent
