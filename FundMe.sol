34a122c2fe448debf1d4adbe215b541d527c2029
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-08 15:00:59 +0530

    getprice from chainlink

--- FundMe.sol
+++ FundMe.sol
@@ -5,16 +5,37 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe{
+    uint256 public minimumUsd = 5;//Instead of minimum ETH, let's set minimum Usd value
+
     function fund() public payable{//1. How do we send ETH to this contract? - Use "payable"
         // Allow users to send $
         // Have a minimum $ sent
-        require(msg.value>1e18, "didn't send enough ETH");// 1e18 = 1ETH = 1000000000000000000 = 1 * 10 ** 18
-
+        require(msg.value >= minimumUsd, "didn't send enough ETH");// 1e18 = 1ETH = 1000000000000000000 = 1 * 10 ** 18
         // What is a revert?
         // Undo any actions that have been done, and send the remaining gas back
         // Even if the transaction reverts, since some computation occured. Gas is spent
     }
+    
+    // Using Oracle to get the price of ETH/USD
+    function getPrice() public view returns (uint256){
+        //Address - 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //ABI 
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+         (
+            /* uint80 roundID */,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return uint256(price) * 1e10;
+    }
+
+    function getConversionRate() public {}
 
     function withdraw() public{}
+    
 }
